c353a81c6e069276258b578bf8f3fdd434572774
CondenadoDev <Camiloosoriodev@gmail.com>
2024-12-13 11:27:08 -0300

    prueba server

--- src/pages/SpreadsheetPage.jsx
+++ src/pages/SpreadsheetPage.jsx
@@ -30,6 +30,7 @@ const SpreadsheetPage = () => {
         fetchClientData();
     }, [clientId]);
 
+
     return (
         <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
             <Sidebar />
